f5e77262907fabd20774d83fdf70b47ee9320550
Prathamesh more <91453437+iamprathameshmore@users.noreply.github.com>
2023-07-10 20:37:51 +0530

    Update problem01.py
    make to solved in two different ways

--- problem01.py
+++ problem01.py
@@ -1,20 +1,38 @@
-'''Write a Python program to calculate the difference between a given number and 17.
-If the number is greater than 17, return twice the absolute difference.'''
+# 1st way to solve  
+
+''' Write a Python program to calculate the difference between a given number and 17.
+If the number is greater than 17, return twice the absolute difference. '''
 
 a = 17 
 b = int(input("enter the number to calculate the diffrence between a given number and 17 : "))
 
+
 if b != 0 : 
     if a == b:
-        print(" both the values are same ");
-    
-    elif a > b :
-        print (" the value is less than 17 and the compasrion between the numbers is", a-b ) 
+        c=(" both the values are same ");
         
-    elif a < b :
-        print (" the value is greater than 17 and the compasrion between the numbers is", b-a)
-        
+    elif b <= 17 : 
+        c=(" the value is less than 17 and the compasrion between the numbers is", a-b ) 
+           
+    else:
+        c=((b-17)*2)
 else :
-    print (" inpute value is less than zero and the compasrion between the numbers is", a-b)
+     c=(" input value is less than zero and the compasrion between the numbers is", a-b)
+
+print ("1st way to solve ,", c)     
     
     
+# 2nd way to solve   
+''' Write a Python program to calculate the difference between a given number and 17.
+If the number is greater than 17, return twice the absolute difference. '''
+
+print ("\n2nd way to solve")
+def difference(b):
+    if b <= 17:
+        return 17 - b
+    else:
+        return (b - 17) * 2 
+
+print( difference(b))
+
+
